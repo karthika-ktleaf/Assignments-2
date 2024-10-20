58ad6e83209415d07617a415025e15837b83b148
karthika-ktleaf <karthika.coax@gmail.com>
2024-10-20 22:53:21 +0530

    Create LaunchBrowser.java

---
+++ program/LaunchBrowser.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Launchbrowser{
+
+	public static void main(String[] args) {
+		  ChromeDriver driver=new ChromeDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+											}
+							}
